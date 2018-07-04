61c19f2137085dbb25d1408c1fb1c9e9ed61c97d
Shencs <34359927+shencss@users.noreply.github.com>
2018-07-04 22:53:10 +0800

    canvas实现滤镜效果

--- Canvas滤镜/main.js
+++ Canvas滤镜/main.js
@@ -1,87 +1,89 @@
-//获取画布
-var canvas = document.getElementById('myCanvas'),
-	ctx = canvas.getContext('2D'),
-	canvasWidth = 320,
-	canvasHeight = 480,
-	radius = 35,
-	clipRegion = {x:-1,y:-1,r:radius},
-	//获取按键
-	show = document.getElementById('show'),
-	reset = document.getElementById('reset'),
-	img,
-	timeId,
-	max = Math.max(canvasWidth,canvasHeight)*2;
-//按键监听事件
-show.addEventListener('click',function(){
-	showChange();
+//获取canvas
+var canvas = document.getElementById("myCanvas"),
+    ctx = canvas.getContext("2d"),
+    //规定尺寸
+    canvasWidth = 320,
+    canvasHeight = 480,
+    radius = 35,
+    //位置
+    clipRegion = {x: -1, y: -1, r: radius},
+    //获取按钮
+    show = document.getElementById("show"),
+    resetButton = document.getElementById("reset"),
+
+    img,
+    timeId,
+    max = Math.max(canvasWidth, canvasHeight) * 2;
+//绑定监听事件
+show.addEventListener("click", function() {
+    showChange();   
 },false);
+resetButton.addEventListener("click", function() {
+    reset();
+}, false);
 
-reset.addEventListener('click',function(){
-	reset();
-})
-
-
+//除去滤镜
 function showChange() {
-	timeId = setTimeout(function change() {
-		if(clipRegion.r > max){
-			stopChange(timeId);
-		}else{
-			clipRegion.r += 10;
-			draw(img,clipRegion);
-			timeId = setTimeout(change,30);
-			console.log('1');
-		}
-	},30);
+    timeId = setTimeout(function change() {
+        if(clipRegion.r > max) {
+            stopChange(timeId);
+        } else {
+            clipRegion.r += 10;
+            draw(img, clipRegion);
+            timeId = setTimeout(change, 30);
+            console.log("1");
+        }
+    }, 30);
 }
 
-
-function stopChange(timeId){
-	clearTimeout(timeId);
+//停止timeout
+function stopChange(timeId) {
+    clearTimeout(timeId);
 }
 
+//初始化
+function initView() {
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+    setRandomPosition();
 
-function initView(){
-	canvas.width = canvasWidth;
-	canvas.height = canvasHeight;
-	setRandomPosition();
-	img = new Image();
-	img.src = 'spider.jpg';
-	img.onload = function(e){
-		draw();
-	}
+    img = new Image();
+    img.src = "spider.jpg";
+    img.onload = function(e) {
+        draw();
+    }
 }
 
-function reset(){
-	stopChange(timeId);
-	clipRegion.r = radius;
-	setRandomPoistion();
-	draw();
+//重置
+function reset() {
+    stopChange(timeId);
+    clipRegion.r = radius;
+    setRandomPosition();
+        draw();
 }
 
-function draw(){
-	ctx.clearRect(0,0,canvasWidth,canvasHeight);
-	ctx.save();
-	drawClip();
-	ctx.drawIamge(img,0,0,canvasWidth,canvasHeight);
-	ctx.restore();
+//画出同心圆图像
+function draw() {
+
+    ctx.clearRect(0,0,canvasWidth, canvasHeight);
+    ctx.save();
+    drawClip();
+    ctx.drawImage(img, 0, 0, canvasWidth, canvasHeight);
+    ctx.restore();
+
 }
 
-function drawClip(){
-	ctx.beginPath();
-	ctx.arc(clipRegion.x,clipRegion.y,clipRegion.r,0,Math.PI*2,false);
-	ctx.clip();
+//获取同心圆
+function drawClip() {
+    ctx.beginPath();
+    ctx.arc(clipRegion.x, clipRegion.y, clipRegion.r, 0, Math.PI * 2, false);
+    ctx.clip();
 }
 
-function setRandomPosition(){
-	clipRegion.x = Math.random() * (canvasWidth - 2 * radius) + radius;
-	clipRegion.y = Math.random() * (canvasHeight - 2 * radius) + radius;
+//随机放置圆心
+function setRandomPosition() {
+    clipRegion.x = Math.random() * (canvasWidth - 2 * radius) + radius;
+    clipRegion.y = Math.random() * (canvasHeight - 2* radius) + radius;
 }
-
 
 initView();
-
-
-
-
-
-
